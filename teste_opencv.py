b8d2a9cc035d11f8ef98d079a8ac744e2f04b2ff
VonIgnia <giovanniar@al.insper.edu.br>
2023-09-29 20:55:48 -0300

    intalando bibliotecas v2

--- teste_opencv.py
+++ teste_opencv.py
@@ -1,3 +1,11 @@
+import numpy 
+print (numpy.__version__)
 
 import cv2
-cv2.__version__
+print (cv2.__version__)
+
+import matplotlib.pyplot as plt
+import scipy
+import pandas as pd
+
+print ("Tudo ok")
