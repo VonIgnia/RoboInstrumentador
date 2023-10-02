eb45f889fb671bdb0a2fd9fa9ee549b32dfece12
VonIgnia <giovanniar@al.insper.edu.br>
2023-10-02 16:10:04 -0300

    Usando camera da rasp: funcionou

--- picamera_test.py
+++ picamera_test.py
@@ -1,17 +1,16 @@
 import cv2
-#import mediapipe
-
-print (cv2.__version__)
-
-cap = cv2.VideoCapture(0)
+from picamera2 import Picamera2
+piCam = Picamera2()
+piCam.preview_configuration.main.size = (1280,720)
+piCam.preview_configuration.main.format = "RGB888"
+piCam.preview_configuration.align()
+piCam.configure("preview")
+piCam.start()
 while True:
-    ret, frame = cap.read()
-    if ret:
-        cv2.imshow('frame', frame)
-        if cv2.waitkey(1):
-            break
-    else:
-        print("no return")
+    frame = piCam.capture_array()
+    cv2.imshow("piCam", frame)
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-cap.release()
 cv2.destroyAllWindows()
